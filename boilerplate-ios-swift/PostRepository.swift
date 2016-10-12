351d9ccb4a288001c1dc4e15b5b60b19d3424ef6
gaillota <manzagregine@gmail.com>
2016-10-12 17:24:02 +0200

    First commit

---
+++ boilerplate-ios-swift/PostRepository.swift
@@ -0,0 +1,9 @@
+//
+//  PostRepository.swift
+//  boilerplate-ios-swift
+//
+//  Created by agaillot on 12/10/2016.
+//  Copyright © 2016 agaillot. All rights reserved.
+//
+
+import Foundation
---
+++ boilerplate-ios-swift/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  boilerplate-ios-swift
+//
+//  Created by agaillot on 12/10/2016.
+//  Copyright © 2016 agaillot. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
